f678ab59c52f793819ac1e80200344ec5265c8a6
cuttice <odhiambocuttice@gmail.com>
2019-08-05 11:25:23 +0300

    push

--- curtisproject/settings.py
+++ curtisproject/settings.py
@@ -92,6 +92,18 @@ WSGI_APPLICATION = 'curtisproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'KEY_PREFIX': 'myapp',
+        'LOCATION': 'redis://127.0.0.1:6379',
+        'OPTIONS':{
+            'CLIENT_CLASS':'django_redis.client.DefaultClient',
+        }
+    }
+}
+CACHE_TTL = 60 * 15
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
--- hello/views.py
+++ hello/views.py
@@ -2,14 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from hello.forms import ProjectForm
 from hello.models import Project
+from django.conf import settings
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+from django.views.decorators.cache import cache_page
 
 
 # Create your views here.
 
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
+
+@cache_page(CACHE_TTL)
 def hello_world(request, *args, **kwargs):
     # return HttpResponse("<h1>hello world</h1>")
     return render(request, "hello_world.html", {})
+
 
 def project_views(request):
     context = {}
