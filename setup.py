7e46f07121f65bb0623ee5bda0a3fc157accf014
cuttice <odhiambocuttice@gmail.com>
2019-08-05 18:13:52 +0300

    Host file correction #14

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='curtislast',
-    version='1.0.13',
+    version='1.0.14',
     description='lists the types of coffee',
     long_description=long_description,
     long_description_content_type='text/markdown',
