104f67c4034b58d65f7babd88f0de9da3864bb29
cuttice <odhiambocuttice@gmail.com>
2019-08-05 14:09:19 +0300

    commenting of config file

--- hello/tests/test_forms.py
+++ hello/tests/test_forms.py
@@ -38,12 +38,12 @@ class TestUrls(SimpleTestCase):
         self.assertEqual(resolve(url).func, project_views)
 
 
-def test_an_admin_view(admin_client):
-    response = admin_client.get('/admin/')
-    assert response.status_code == 200
-class TestProjectAdmin:
-	def test_expect(self):
-		 site = AdminSite()
+# # def test_an_admin_view(admin_client):
+# #     response = admin_client.get('/admin/')
+# #     assert response.status_code == 200
+# class TestProjectAdmin:
+# 	def test_expect(self):
+# 		 site = AdminSite()
 
 
 class TestForms(SimpleTestCase):
